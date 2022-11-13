d22f1c8d01de9e94647df7e3ce2644f4a4681227
Jared Cosulich <jared.cosulich@gmail.com>
2022-11-13 08:29:35 -0500

    trying to get github pages working

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  basePath: '/checker-react',
+  assetPrefix: '/checker-react', 
 }
 
 module.exports = nextConfig
